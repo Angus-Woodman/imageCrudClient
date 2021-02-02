9d2ce91eca667daf98773b2adce7983c4edd4771
Angus Woodman <>
2021-02-02 17:46:24 +0000

    add emoji functionality

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AddImage from "../Components/AddImage"
+import '../styles/HomePage.css'
 
 class HomePage extends Component {
     state = {imgData:[{avatar:''}]};
@@ -28,16 +29,39 @@ class HomePage extends Component {
       .catch(console.warn)
     }
 
+    handleEmoji = (e, idx) => {
+      let urlBackend = `https://img-crud.herokuapp.com/user/${}`;
+      // let urlBackend = `http://localhost:5000/user/${this.state.imgData[idx]._id}`   
+      let emoji = e.target.className
+      
+      let value = parseInt(this.state.imgData[idx].[emoji])+1
+
+      const parseData = new FormData()
+      parseData.append(emoji, value.toString())
+      console.log(value)
+      console.log(emoji)
+
+      let options = {
+        method:'PUT',
+        body:parseData
+    }
+
+      fetch(urlBackend, options)
+      .then(r => r.json())
+      .catch(console.warn)
+
+    }
+
     render() {
       return (
         <>
           <h1>Client side of Image Crud practice application</h1>
-          {this.state.imgData.map((item,idx) => (<div key={idx}>
+          {this.state.imgData.map((item,idx) => (<div key={idx} className='card'>
             <img style={{width: 300}} src={item.avatar}></img>
             <p>name: {item.name}</p>
             <p>Camera: {item.camera}</p>
             <p>Lens: {item.lens}</p>
-            <p>emojis: {item.emojis}</p>
+            <p>emojis: <button className='emoji1' onClick={(e) => this.handleEmoji(e, idx)}>😀</button> {item.emoji1} <button className='emoji2' onClick={(e) => this.handleEmoji(e, idx)}>👍</button> {item.emoji2} <button className='emoji3' onClick={(e) => this.handleEmoji(e, idx)}>💘</button>{item.emoji3}</p>
             <p>comments: {item.comments}</p>
             <button onClick={() => this.handleDelete(item._id)}>Delete</button>
             </div>
